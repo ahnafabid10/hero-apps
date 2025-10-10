44bb23be85239a6adbef141cc15e532e81a9a3f0
Ahnaf Abid <ahnafabid600@gmail.com>
2025-10-11 01:37:35 +0600

    first commit

---
+++ src/Components/Routes/Routes.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
+
+<RouterProvider router={router} />
