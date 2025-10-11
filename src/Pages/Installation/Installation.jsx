14d1d8ad5c2ee227d6b03396b72b8283f5146700
Ahnaf Abid <ahnafabid600@gmail.com>
2025-10-12 02:27:27 +0600

    first commit

--- src/Pages/Installation/Installation.jsx
+++ src/Pages/Installation/Installation.jsx
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Installation = () => {
+    const [install, setInstall] = useState([])
+    useEffect(() =>{
+        const savedList = JSON.parse(localStorage.getItem('Installed'))
+        if(savedList) setInstall (savedList)
+    }, [])
     return (
-        <div>
+        <div className='space-y-6'>
+            <div>
+                <h1 className='text-4xl font-bold'>Your Installed Apps</h1>
+                <p className='text-[#627382]'>Explore All Trending Apps on the Market developed by us</p>
+            </div>
+            <div className='flex justify-between'>
+                <h2>savedList: {install.length}</h2>
+                <p>Sorted By Size</p>
+            </div>
+
+            <div>
+                <div className='flex justify-between'>
+                    <img src="" alt="" />
+                </div>
+                
+                
+                
+                <div>
+                    <div className='flex'>
+                        <div>
+                            <h1>title</h1>
+                        </div>
+
+                        <div className='flex'>
+                            <p>downloads</p>
+                            <p>downloads</p>
+                            <p>downloads</p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <button>Uninstall</button>
+                    </div>
+                </div>
+               
+            </div>
             
         </div>
     );
